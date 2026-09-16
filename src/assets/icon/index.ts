4559952a3c7c9c7f3121f1637be2c656c56f646c
Vinh Nguyen <nguyenvinh0712@gmail.com>
2026-09-16 13:59:41 +0700

    Vinh | icon on main

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,79 @@
+import arrowLeft from './arrow-left.svg'
+import arrowRight from './arrow-right.svg'
+import calendar from './calendar.svg'
+import cart from './cart.svg'
+import cash from './cash.svg'
+import check from './check.svg'
+import chevronDown from './chevron-down.svg'
+import chevronLeft from './chevron-left.svg'
+import chevronRight from './chevron-right.svg'
+import clock from './clock.svg'
+import close from './close.svg'
+import creditCard from './credit-card.svg'
+import edit from './edit.svg'
+import eye from './eye.svg'
+import eyeOff from './eye-off.svg'
+import filter from './filter.svg'
+import food from './food.svg'
+import headset from './headset.svg'
+import heart from './heart.svg'
+import home from './home.svg'
+import location from './location.svg'
+import lock from './lock.svg'
+import logout from './logout.svg'
+import mail from './mail.svg'
+import menu from './menu.svg'
+import minus from './minus.svg'
+import phone from './phone.svg'
+import plus from './plus.svg'
+import receipt from './receipt.svg'
+import search from './search.svg'
+import shieldCheck from './shield-check.svg'
+import star from './star.svg'
+import tag from './tag.svg'
+import trash from './trash.svg'
+import truck from './truck.svg'
+import user from './user.svg'
+import wallet from './wallet.svg'
+
+export const icons = {
+  arrowLeft,
+  arrowRight,
+  calendar,
+  cart,
+  cash,
+  check,
+  chevronDown,
+  chevronLeft,
+  chevronRight,
+  clock,
+  close,
+  creditCard,
+  edit,
+  eye,
+  eyeOff,
+  filter,
+  food,
+  headset,
+  heart,
+  home,
+  location,
+  lock,
+  logout,
+  mail,
+  menu,
+  minus,
+  phone,
+  plus,
+  receipt,
+  search,
+  shieldCheck,
+  star,
+  tag,
+  trash,
+  truck,
+  user,
+  wallet,
+}
+
+export type IconName = keyof typeof icons
